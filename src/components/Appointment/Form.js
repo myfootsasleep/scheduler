a9196a0e6d896915f59c8e7c77ab3bd087f5dfef
myfootsasleep <richardoda@gmail.com>
2023-08-07 11:56:05 -0700

    made changes to the Form.js, checked that everything was working

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -8,27 +8,37 @@ export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
   
+  const reset = () => {
+    setStudent(""); // Reset the student state
+    setInterviewer(null); // Reset the interviewer state
+  };
+  const cancel = (props) => {
+    reset(); // Call the reset function to reset the form
+    props.onCancel()
+  };
   return (
     
     <main className="appointment__card appointment__card--create">
     <section className="appointment__card-left">
-      <form autoComplete="off">
+      <form autoComplete="off" onSubmit={event => event.preventDefault()}>
         <input
           className="appointment__create-input text--semi-bold"
           name="name"
           type="text"
           placeholder="Enter Student Name" 
-          value = {props.student}
+          value = {student}
+          onChange={(event) => setStudent(event.target.value)}
         />
       </form>
       <InterviewerList
           interviewers={props.interviewers}
-          value={props.value}
+          value={interviewer}
+          onChange = {setInterviewer}
         />
     </section>
     <section className="appointment__card-right">
       <section className="appointment__actions">
-        <Button danger onClick = {props.onCancel}>Cancel</Button>
+        <Button danger onClick = {cancel}>Cancel</Button>
         <Button confirm onClick ={props.onSave}>Save</Button>
       </section>
     </section>
